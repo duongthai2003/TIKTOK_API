94247caa876fd4ca4bc174bf30752b77a5357e94
duongthai2003 <thaiq9577@gmail.com>
2024-11-04 09:57:14 +0700

    hggnbvbnv

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,7 +44,7 @@
                         </g>
                     </svg>
                 </div>
-
+                <img src="{{ asset('images/img.jpg') }}" alt="My Image">
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
